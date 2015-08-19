2b965f31811ee42062db260862f9692ec40d0981
pasonx <619765945@qq.com>
2015-08-19 15:40:21 +0800

    second submit

--- app/src/main/java/com/pason/justtest/ChangeColorIconWithText.java
+++ app/src/main/java/com/pason/justtest/ChangeColorIconWithText.java
@@ -1,7 +1,20 @@
 package com.pason.justtest;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 
 /**
@@ -9,16 +22,179 @@ import android.view.View;
  */
 public class ChangeColorIconWithText extends View {
 
+    private int mColor = 0xFF45C01A;
+    private Bitmap mIconBitmap;
+    private int mTextSize = (int) TypedValue
+            .applyDimension(TypedValue
+                    .COMPLEX_UNIT_SP, 12, getResources()
+                    .getDisplayMetrics());
+    private String mText = "微信";
+
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Paint paint;
+    private float mAlpha;
+
+    private Rect iconRect;
+    private Rect textBound;
+    private Paint textPaint;
+
+    private static final String INSTANCE_STATUS = "instance_status";
+    private static final String STATUS_ALPHA = "status_alpha";
 
     public ChangeColorIconWithText(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public ChangeColorIconWithText(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
+
+    /**
+     * 获取自定义属性的值
+     *
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     */
     public ChangeColorIconWithText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ChangeColorIconWithText);
+
+        int n = a.getIndexCount();
+
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.ChangeColorIconWithText_icon:
+                    BitmapDrawable drawable = (BitmapDrawable) a.getDrawable(attr);
+                    mIconBitmap = drawable.getBitmap();
+                    break;
+                case R.styleable.ChangeColorIconWithText_color:
+                    mColor = a.getColor(attr, 0xFF45C01A);
+                    break;
+                case R.styleable.ChangeColorIconWithText_text:
+                    mText = a.getString(attr);
+                    break;
+                case R.styleable.ChangeColorIconWithText_text_size:
+                    mTextSize = a.getDimensionPixelSize(attr, (int) TypedValue
+                            .applyDimension(TypedValue
+                                    .COMPLEX_UNIT_SP, 12, getResources()
+                                    .getDisplayMetrics()));
+                    break;
+
+
+            }
+        }
+        a.recycle();
+
+        textBound = new Rect();
+        textPaint = new Paint();
+        textPaint.setTextSize(mTextSize);
+        textPaint.setColor(0Xff555555);
+        textPaint.getTextBounds(mText, 0, mText.length(), textBound);
+
+
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int iconWidth = Math.min(getMeasuredWidth() - getPaddingLeft()
+                - getPaddingRight(), getMeasuredHeight() - getPaddingTop()
+                - getPaddingBottom() - textBound.height());
+
+
+        int left = (getMeasuredWidth() - iconWidth) / 2;
+        int top = (getMeasuredHeight() - (iconWidth + textBound.height())) / 2;
+        iconRect = new Rect(left,top,left + iconWidth,top + iconWidth);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(mIconBitmap, null, iconRect, null);
+
+        int alpha = (int) Math.ceil(255 * mAlpha);
+        //准备绘制纯色，设置透明度，纯色，Xfermode,图标
+        setTargetBitmap(alpha);
+        //绘制底侧文本
+        drawSouceText(canvas,alpha);
+        //绘制变色文本
+        drawTargetText(canvas,alpha);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+
+    }
+
+    private void drawTargetText(Canvas canvas, int alpha) {
+        textPaint.setColor(mColor);
+        textPaint.setAlpha(alpha);
+        int x = getMeasuredWidth() / 2 - textBound.width() / 2;
+        int y = iconRect.bottom + textBound.height();
+        canvas.drawText(mText, x, y, textPaint);
+    }
+
+    /**
+     * 绘制原文本
+     *
+     */
+    private void drawSouceText(Canvas canvas,int alpha) {
+        textPaint.setColor(0xff333333);
+        textPaint.setAlpha(255 - alpha);
+        int x = getMeasuredWidth() / 2 - textBound.width() / 2;
+        int y = iconRect.bottom + textBound.height();
+        canvas.drawText(mText, x, y, textPaint);
+    }
+
+    private void setTargetBitmap(int alpha) {
+        bitmap = bitmap.createBitmap(getMeasuredWidth(),getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        paint = new Paint();
+        paint.setColor(mColor);
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setAlpha(alpha);
+
+        canvas.drawRect(iconRect, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+
+        paint.setAlpha(255);
+        canvas.drawBitmap(mIconBitmap, null, iconRect, paint);
+
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(INSTANCE_STATUS,super.onSaveInstanceState());
+        bundle.putFloat(STATUS_ALPHA,mAlpha);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Bundle)
+        {
+            Bundle bundle = (Bundle)state;
+            mAlpha = bundle.getFloat(STATUS_ALPHA);
+            super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_STATUS));
+            return;
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    public void setIconAlpha(float alpha){
+        mAlpha = alpha;
+        invalidateView();
+    }
+
+    private void invalidateView() {
+        if (Looper.getMainLooper() == Looper.myLooper()){
+            invalidate();
+        }else {
+            postInvalidate();
+        }
+    }
+
 }
--- app/src/main/java/com/pason/justtest/MainActivity.java
+++ app/src/main/java/com/pason/justtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.pason.justtest;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -9,15 +10,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener,ViewPager.OnPageChangeListener{
 
     private ViewPager vp;
 
     private List<Fragment> mTabs = new ArrayList<Fragment>();
+
+    private List<ChangeColorIconWithText> mBtnTabs = new ArrayList<ChangeColorIconWithText>();
 
     private String[] mTitles = new String[]{"First Fragment","Second Fragment","Third Fragment","Fourth Fragment"};
 
@@ -30,12 +34,31 @@ public class MainActivity extends FragmentActivity {
         initView();
         initData();
         vp.setAdapter(mAdapter);
+        initEvent();
+    }
+
+    private void initEvent() {
+        vp.setOnPageChangeListener(this);
     }
 
 
     private void initView(){
-
         vp = (ViewPager)findViewById(R.id.vpager);
+        ChangeColorIconWithText one = (ChangeColorIconWithText) findViewById(R.id.ind_one);
+        mBtnTabs.add(one);
+        ChangeColorIconWithText two = (ChangeColorIconWithText) findViewById(R.id.ind_two);
+        mBtnTabs.add(two);
+        ChangeColorIconWithText three = (ChangeColorIconWithText) findViewById(R.id.ind_three);
+        mBtnTabs.add(three);
+        ChangeColorIconWithText four = (ChangeColorIconWithText) findViewById(R.id.ind_four);
+        mBtnTabs.add(four);
+
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+
+        one.setIconAlpha(1.0f);
     }
 
     private void initData(){
@@ -77,4 +100,57 @@ public class MainActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        reSetOtherBtnTabs();
+        switch (v.getId())
+        {
+            case R.id.ind_one:
+                mBtnTabs.get(0).setIconAlpha(1.0f);
+                vp.setCurrentItem(0,false);
+                break;
+            case R.id.ind_two:
+                mBtnTabs.get(1).setIconAlpha(1.0f);
+                vp.setCurrentItem(1, false);
+                break;
+            case R.id.ind_three:
+                mBtnTabs.get(2).setIconAlpha(1.0f);
+                vp.setCurrentItem(2, false);
+                break;
+            case R.id.ind_four:
+                mBtnTabs.get(3).setIconAlpha(1.0f);
+                vp.setCurrentItem(3, false);
+                break;
+
+        }
+    }
+
+    private void reSetOtherBtnTabs() {
+        for (int i = 0 ; i < mBtnTabs.size(); i ++)
+        {
+            mBtnTabs.get(i).setIconAlpha(0);
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        if (positionOffset > 0)
+        {
+            ChangeColorIconWithText left = mBtnTabs.get(position);
+            ChangeColorIconWithText right = mBtnTabs.get(position + 1);
+            left.setIconAlpha(1 - positionOffset);
+            right.setIconAlpha(positionOffset);
+        }
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
 }
